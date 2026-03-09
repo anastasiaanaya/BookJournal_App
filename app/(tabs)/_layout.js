895524e51e6321f265c52ee522ec851df0fd9851
anastasiaanaya <anastasia.anaya@estudiantat.upc.edu>
2026-03-09 10:05:26 +0100

    _layout tabs&stacks

---
+++ app/(tabs)/_layout.js
@@ -0,0 +1,76 @@
+import { Tabs } from 'expo-router';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function Layout() {
+  return (
+    
+    <Tabs
+      screenOptions={{
+        // Estils Header
+        headerStyle: { backgroundColor: '#25292e' },
+        headerTintColor: '#fff',
+        headerTitleStyle: { fontWeight: 'bold' },
+        
+        // Colors menú inferior
+        tabBarActiveTintColor: '#8B4513',
+        tabBarInactiveTintColor: 'gray',
+      }}
+    >
+      {/* Pestanya Inici */}
+      <Tabs.Screen 
+        name="index" // 'index' es la pantalla principal (Home)
+        options={{ 
+          title: 'Inici',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color} />
+          )
+        }} 
+      />
+
+      {/* 2. Pestanya Biblioteca */}
+      <Tabs.Screen 
+        name="biblioteca" 
+        options={{ 
+          title: 'Biblioteca',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'library' : 'library-outline'} size={size} color={color} />
+          )
+        }} 
+      />
+      
+
+      {/* 3. Pestanya Explorar */}
+      <Tabs.Screen 
+        name="explorar" 
+        options={{ 
+          title: 'Explorar',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'search' : 'search-outline'} size={size} color={color} />
+          )
+        }} 
+      />
+
+      {/* 4. Pestanya Diari */}
+      <Tabs.Screen 
+        name="diari" 
+        options={{ 
+          title: 'El meu Diari',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'book' : 'book-outline'} size={size} color={color} />
+          )
+        }} 
+      />
+
+      {/* 5. Pestanya Perfil */}
+      <Tabs.Screen 
+        name="perfil" 
+        options={{ 
+          title: 'Perfil',
+          tabBarIcon: ({ focused, color, size }) => (
+            <Ionicons name={focused ? 'person' : 'person-outline'} size={size} color={color} />
+          )
+        }} 
+      />
+    </Tabs>
+  );
+}
